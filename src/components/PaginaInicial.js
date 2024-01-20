e7f06c318f4319bf588d415c9cd5490aea32c95d
ViniciusBenetti <makako1289@gmail.com>
2024-01-20 13:59:54 -0300

    d

--- src/components/PaginaInicial.js
+++ src/components/PaginaInicial.js
@@ -14,6 +14,12 @@ export default function PaginaInicial(){
             </p>
             <img id="calc"  alt='Calculadora' src='https://viniciusbenetti.github.io/web-src/Calculadora1.png'/>
 
+            <h2>COMO USAR?</h2>
+            <p>Para usar o aplicativo, primeiro vá até "calculadoras" e selecione a calculadora desejada.
+                Para cada calculadora, digite os valores de entrada e aperte em "calcular", o resultado irá
+                sair no último campo após pressionado o botão.
+            </p>
+
         </div>
         
     )
