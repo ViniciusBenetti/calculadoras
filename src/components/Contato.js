cc19f8b89ec943034ce1f46e086bff4a1c34182f
ViniciusBenetti <makako1289@gmail.com>
2024-01-20 15:47:49 -0300

    a

--- src/components/Contato.js
+++ src/components/Contato.js
@@ -6,13 +6,13 @@ export default function Contato(){
             
             <h1>CONTATO</h1>
             <a href="https://instagram.com/vinix_dev" target="_blank">
-            <img src="../../public/instagram.png" alt="insta"/>
+            <img src="https://viniciusbenetti.github.io/web-src/instagram.png" alt="insta"/>
             </a>
             <a href="https://www.linkedin.com/in/vinicius-benetti-costa/" target="_blank">
-            <img src="../../public/linkedin.png" alt="linkedin"/>
+            <img src="https://viniciusbenetti.github.io/web-src/linkedin.png" alt="linkedin"/>
             </a>
             <a href="https://www.facebook.com/profile.php?id=61553699226818" target="_blank">
-            <img src="../../public/facebook.png" alt="fb"/>
+            <img src="https://viniciusbenetti.github.io/web-src/facebook.png" alt="fb"/>
             </a>
 
         </div>  
